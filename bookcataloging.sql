0108cbbc5f3f35a92a49938bdc08ffad08adf0ba
Mahdy Dellawari <124960226+MahdyDellawari@users.noreply.github.com>
2023-11-17 13:16:28 -0500

    Update bookcataloging.sql

--- bookcataloging.sql
+++ bookcataloging.sql
@@ -1,28 +1,26 @@
 --
--- Database: `demo` and php web application user
-CREATE DATABASE demo;
-GRANT USAGE ON *.* TO 'appuser'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON demo.* TO 'appuser'@'localhost';
-FLUSH PRIVILEGES;
- 
-USE demo;
+-- Database: `bookcataloging` and php web application user
+CREATE DATABASE bookcataloging;
+GRANT ALL PRIVILEGES ON bookcataloging.* TO 'Mohammad'@'localhost' WITH GRANT OPTION;
+
+USE bookcataloging;
 --
--- Table structure for table `employees`
+-- Table structure for table `Books`
 --
  
-CREATE TABLE IF NOT EXISTS `bookcataloging` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `BookName` varchar(100) NOT NULL,
   `AuthorName` varchar(255) NOT NULL,
   `Edition` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
  
 --
--- Dumping data for table `employees`
+-- Dumping data for table `Books`
 --
  
-INSERT INTO `BookCataloging` (`Id`, `BookName`, `AuthorName`, `Edition`) VALUES
+INSERT INTO `Books` (`id`, `BookName`, `AuthorName`, `Edition`) VALUES
 (1, 'The Great Gatsby', 'F. Scott Fitzgerald', 'First Edition'),
 (2, 'To Kill a Mockingbird', 'Harper Lee', 'Second Edition'),
 (3, '1984', 'George Orwell', 'Revised Edition'),

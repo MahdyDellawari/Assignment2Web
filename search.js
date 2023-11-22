1ff19280c42740f68351f8bba5bf07e089831c1b
seyedehmahsa <151188439+seyedehmahsa@users.noreply.github.com>
2023-11-22 15:12:14 -0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,8 @@
+function search() {
+    // Get the search input value
+    var searchTerm = document.getElementById("searchInput").value;
+
+    // Perform a search (you can customize this part based on your needs)
+    // For simplicity, let's just display the search term for now
+    document.getElementById("searchResults").innerHTML = "Search Results: " + searchTerm;
+}

d9dab75ec2380c0b8d75588c0b3d2cb7a56b152f
AramEsmaeili <151187793+AramEsmaeili@users.noreply.github.com>
2023-11-22 17:07:10 -0500

    Delete search.js

--- search.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    // Get the search input value
-    var searchTerm = document.getElementById("searchInput").value;
-
-    // Perform a search (you can customize this part based on your needs)
-    // For simplicity, let's just display the search term for now
-    document.getElementById("searchResults").innerHTML = "Search Results: " + searchTerm;
-}
